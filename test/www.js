a65380362a702b495280c32cdbde2744a5fcaa82
Darron Froese <darron@froese.org>
2014-01-16 22:59:52 -0700

    Updated site.

--- test/www.js
+++ test/www.js
@@ -2,7 +2,7 @@ module.exports = {
 'Main octohost site test.': function (test) {
   test
     .open('http://www.octohost.io')
-    .assert.title().is('octohost', 'It has title')
+    .assert.title().is('octohost - Simple web focused Docker based mini-PaaS server.', 'It has title')
     .done();
 }
 };
