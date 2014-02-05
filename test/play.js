0b42b45a083d36da1c0247b0ad3ce9a781787279
Darron Froese <darron@froese.org>
2014-02-05 15:01:14 -0700

    Removing play test.

--- test/play.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-'Play Framework tests': function (test) {
-  test
-    .open('http://play-app.octohost.io/')
-    .assert.title().is('Play Framework on octohost', 'It has title')
-    .assert.text('.jumbotron h1').is('Play Framework', 'It has an H1')
-    .done();
-}
-};
