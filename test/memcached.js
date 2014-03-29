9915f432ff58ae32017f0a20edb5cf94fb8d4336
Darron Froese <darron@froese.org>
2014-03-29 11:42:20 -0600

    Removing Redis and Memcached tests.

--- test/memcached.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-'Memcached tests': function (test) {
-  test
-    .open('http://memcached.octohost.io/')
-    .assert.title().is('Persistent containers on octohost.', 'It has title')
-    .assert.text('h2').is('Memcache backed Hit Counter', 'It has an H2')
-    .done();
-}
-};
--- test/redis.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-'Redis tests': function (test) {
-  test
-    .open('http://redis.octohost.io/')
-    .assert.title().is('Persistent containers on octohost.', 'It has title')
-    .assert.text('h2').is('Redis backed Hit Counter', 'It has an H2')
-    .done();
-}
-};
